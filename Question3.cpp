fdde8513b8b84c79ab2d771f482707f38a479a0e
213562404 <david.theo.parker@gmail.com>
2015-04-13 11:03:31 +0200

    made some changes

--- Question3.cpp
+++ Question3.cpp
@@ -22,6 +22,33 @@ private:
 	int num;				// numerator;
 	int denom;				// denominator;
 public:
-	Fraction(int n, int d) : num(n), denum(d) { };
+	Fraction(){};
+	~Fraction(){};
+	Fraction(int n, int d) : num(n), denom(d) { };
 	void print() { cout << num << "/" << denom; };
+	friend bool operator>(const Fraction A, const Fraction B);
+	Fraction add(int integer, Fraction frac);
+	Fraction add(Fraction frac, int integer);
 };
+bool operator>(const Fraction A, const Fraction B){
+	int A_num, B_num;
+	A_num = A.num*B.denom;
+	B_num = B.num*A.denom;
+	if (A_num > B_num) 
+		return true;
+	else
+		return false;
+};
+Fraction Fraction::add(Fraction frac, int integer){
+	frac.num += integer*frac.denom;//gets the integer into fraction's base value, then adds it to the numerator
+	return frac;
+};
+Fraction Fraction::add(int integer, Fraction frac){
+	frac.num += integer*frac.denom;//exactly the same method!
+	return frac;
+};
+int main(){
+	//Fraction fracArray[2];
+	//fracA=
+	//fracArray[0] = Fraction()
+}
